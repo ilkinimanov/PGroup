eb955931dd5d8f2dd9199b623465725a6cce57ef
ilkinimanov <coderilkin.03@gmail.com>
2023-07-21 10:56:26 +0400

    index routes added to app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const indexRoutes = require('./routes/indexRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
@@ -8,6 +10,10 @@ const app = express();
 dotenv.config({
   path: './.env'
 })
+
+
+// App routes
+app.use(indexRoutes);
 
 
 module.exports = app;
